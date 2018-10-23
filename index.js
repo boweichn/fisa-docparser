dc24f1460ea3333de6b5d4609e85649f2574a7c1
boweichn <boweicheng@hotmail.com>
2018-10-22 21:16:22 -0700

    initial refactors

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const docparser = require("docparser-node");
-const config = require("./config/config");
+var fs = require('fs');
+var client = new docparser.Client();
 
-var client = new docparser.Client(config.key);
+const myFile = 'test.pdf'
 
+// writes results to JSON
+var writeResult = (myResult) => {
+  fs.writeFile("./results.json", JSON.stringify(myResult, null, 4), (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    };
+    console.log("File has been created");
+  });
+}
+
+// test Auth
 client
   .ping()
   .then(function() {
@@ -12,20 +25,58 @@ client
     console.log("authentication failed!");
   });
 
-client
+// show all parsers
+var grabPaser = () => {
+  client
   .getParsers()
   .then(function(parsers) {
-    console.log(parsers);
-    // => [{"id":"someparserid","label":"My Document Parser"}]
+    console.log(parsers)
   })
   .catch(function(err) {
     console.log(err);
   });
-client
-  .getResultsByParser("uqinwtuacpxs", { format: "object" })
-  .then(function(result) {
-    console.log(result);
+}
+
+// grab result by id
+var returnResult = (id_sequence) => {
+  client
+  .getParsers()
+  .then(function(result){
+    client
+    .getResultsByParser(result[id_sequence].id, { format: "object" })
+    .then(function(result) {
+      console.log(result);
+      writeResult(result);
+    })
+    .catch(function(err) {
+      console.log(err);
+    })
   })
   .catch(function(err) {
     console.log(err);
   });
+}
+
+// upload a document to docParser
+var uploadDoc = (id_sequence) => {
+  client
+  .getParsers()
+  .then(function(result){
+    client
+    .uploadFileByPath(result[id_sequence].id, myFile, {remote_id: 'test'})
+    .then(function (result) {
+      // => {"id":"document_id","file_size":198989,"quota_used":16,"quota_left":34,"quota_refill":"1970-01-01T00:00:00+00:00"}
+      console.log(result)
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+}
+
+var main = () => {
+  returnResult(0);
+}
