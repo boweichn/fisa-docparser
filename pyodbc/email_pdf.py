ad2c99693ff0f1074ea960fc5dde912fc9049697
robertjanzen <robertjanzenbc@gmail.com>
2018-11-18 13:05:58 -0800

    Add new file path for pdfs in email_pdf.py

--- pyodbc/email_pdf.py
+++ pyodbc/email_pdf.py
@@ -17,14 +17,20 @@ def find_pdf(dr, ext):
     return glob(path.join(dr,'*.{}'.format(ext)))
 
 def email_pdfs():
+    # Create database object
     db = retrive_dict()
-    files = find_pdf('.','pdf')
+
+    # Retreive list of pdf filenames
+    files = find_pdf('./prepared_fact_sheets','pdf')
+
+    # Setup email
     msg = MIMEMultipart()
     from_address = 'robertjanzenbc@gmail.com'
     msg['From'] = from_address
     msg['Subject'] = 'FISA BC Fact Sheet'
     body = 'Please electronically fill out the attached form, and return to FISA BC.'
 
+    # Loop through all pdfs in the prepared_fact_sheets folder
     for f in files:
         # Find school name from pdf file name
         school_name = f.split("\\")[1].split(".")[0]
@@ -37,19 +43,23 @@ def email_pdfs():
                 break
 
         # Compose the rest of the email and attach the correct pdf
+        # pdf names will be exactly the same as the school names in the database
         msg.attach(MIMEText(body, 'plain'))
         attachment = open(f, 'rb')
         part = MIMEBase('application', 'octet-stream')
         part.set_payload((attachment).read())
         encoders.encode_base64(part)
-        part.add_header('Content-Disposition', 'attachment; filename= %s' % f)
+        part.add_header('Content-Disposition', 'attachment; filename= %s' % f.split('\\')[1]) # Clean up file name
         msg.attach(part)
 
-        # Send email
+        # Send email, for Gmail you must enable "Allow less secure apps" in security settings
         try:
             server = smtplib.SMTP('smtp.gmail.com', 587)
-            # server.ehlo()
+
+            # Encryption is being used
             server.starttls()
+
+            # Store gmail password in a separate text file called passwords.py
             server.login(from_address, EMAIL_PASS)
             text = msg.as_string()
             server.sendmail(from_address, to_address, text)
